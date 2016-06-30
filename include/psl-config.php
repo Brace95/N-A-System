25154ee174c1d2e85484094979100048be1d0694
Brandon Stenhouse <bstenhouse27@gmail.com>
2016-06-30 15:36:14 +1000

    Removing some stuff

--- include/psl-config.php
+++ include/psl-config.php
@@ -1,15 +1,12 @@
 <?php
 
 	define("HOST", "localhost");
-	define("PASSWORD", "1v25u3f1");
 
-	/* Website */
-	define("USER", "bracespa_mcd");
-	define("DATABASE", "bracespa_na");
+	/* Database */
+	define("USER", "someuser");
+	define("PASSWORD", "somepassword");
+	define("DATABASE", "na_database");
 
-	/* Local */
-	/*define("USER", "root");
-	define("DATABASE", "na");*/
 
 	date_default_timezone_set("Australia/Melbourne");
 
